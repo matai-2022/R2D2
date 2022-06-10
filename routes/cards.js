caa97524d59a1f0eb1bfc4244974aa32939c8926
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-06-10 15:14:58 +1200

    Loop the cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { reset } = require('nodemon')
 
 // const { eventDays, capitalise, validateDay } = require('../helpers')
 const db = require('../db/dbfunctions')
@@ -9,37 +10,59 @@ module.exports = router
 // GET /cards/id
 router.get('/:id', async (req, res) => {
   const cardInfo = await db.getCardById(req.params.id)
-  console.log(cardInfo)
-  res.render('./cards', cardInfo)
+  console.log('cardInfo', cardInfo)
+  res.render('showCard', cardInfo)
 })
 
-// POST /id/select -> /cards/id+1
-router.post('/:id/select', async (req, res) => {
-  const id = req.params.id
+router.get('/select', async (req, res) => {
+  const id = Number(req.body.id)
+  console.log(id)
+  const cardInfo = await db.getCardById(req.body.id + 1)
+  // const cardsLength = await db.getCardsLength()
+  res.render(`/${id + 1}`, cardInfo)
+  // if (id === cardsLength) {
+  //   console.log('cardsLength:', cardsLength)
+  //   res.redirect('/result')
+  // } else {
+  //   console.log('in else')
+  //   res.redirect(`/cards/${id + 1}`)
+  // }
+})
+
+// POST /select -> /cards/id+1
+router.post('/select', async (req, res) => {
+  const id = Number(req.body.id)
   const cardsLength = await db.getCardsLength()
   await db.editCard(id, { selected: 1 })
+  console.log('cardsLength', cardsLength)
   // if last card redirect to results
   // else redirect to next id
   if (id === cardsLength) {
+    console.log('cardsLength:', cardsLength)
     res.redirect('/result')
   } else {
-    res.redirect(`/:${id + 1}`)
+    console.log('in else')
+    res.redirect(`/cards/${id + 1}`)
   }
 })
 
-// POST /id/not-select -> /cards/id+1
-router.post('/:id/not-select', async (req, res) => {
-  const id = req.params.id
+// POST cards/not-select -> /cards/id+1
+router.post('/not-select', async (req, res) => {
+  const id = Number(req.body.id)
   const cardsLength = await db.getCardsLength()
   await db.editCard(id, { selected: 0 })
   // if last card redirect to results
   // else redirect to next id
   if (id === cardsLength) {
+    console.log('here', id)
+
     res.redirect('/result')
   } else {
-    res.redirect(`/:${id + 1}`)
+    console.log('here', id)
+    res.redirect(`/cards/${id + 1}`)
   }
 })
+
 // GET /cards/4/edit
 router.get('/:id/edit', async (req, res) => {
   try {
@@ -63,3 +86,7 @@ router.post('/:id/edit', async (req, res) => {
     res.status(500).send(err.message)
   }
 })
+
+// router.get('/select', async (req, res) => {
+//   res.redirect('/results')
+// })
