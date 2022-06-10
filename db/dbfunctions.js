1324d823464b321a84c23b598113d6f66d7998c6
Jingjing LI <jlkjfk@hotmail.com>
2022-06-10 12:56:26 +1200

    complete editCard

--- db/dbfunctions.js
+++ db/dbfunctions.js
@@ -13,7 +13,16 @@ function getCardById(id,db = connection) {
   return db('cards').where('id', id).first()
 }
 
-function editCard(db = connection) {}
+//function editCard(db = connection) {}
+function editCard(id, updatedCardInfo,db = connection) {
+  return db('cards')
+    .where('id', id)
+    .update(updatedCardInfo)
+
+    .catch((err) => {
+      console.error(err.message)
+    })
+}
 
 function addCard(db = connection) {}
 
--- routes/cards.js
+++ routes/cards.js
@@ -11,3 +11,28 @@ router.get('/:id', async (req, res) => {
   const cardInfo = await db.getCardById(req.params.id)
   res.render('./cards', cardInfo)
 })
+
+// GET /cards/4/edit
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const cardInfo = await db.getCardById(id)
+    res.render('editCard', cardInfo)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+// POST /cards/4/edit
+router.post('/:id/edit', async (req, res) => {
+  
+  try {
+    const { id, name, image } = req.body
+
+   
+    await db.updatedCarInfo(id, name, image)
+    res.redirect('/cards')
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
